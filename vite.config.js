9807ad6d4ef143fde649a1fae449631790ec673c
Shahadat Hossen <shobujlingdu@gmail.com>
2024-12-19 20:08:15 +0600

    Video chat done

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,18 @@ import vue from '@vitejs/plugin-vue';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
+    server: {
+        https: true, // Enable HTTPS
+        hmr: {
+            host: 'factually-clean-pig.ngrok-free.app',
+        },
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
             ssr: 'resources/js/ssr.js',
             refresh: true,
+            detectTls: 'https://factually-clean-pig.ngrok-free.app'
         }),
         vue({
             template: {
